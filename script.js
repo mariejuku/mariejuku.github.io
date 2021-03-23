1855b987e7d4c9d7723b934966388bbdd49e21a8
marie <mariejuku@gmail.com>
2021-03-24 03:31:09 +1100

    blup

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 $( document ).ready(function() {
-    
+
     //set up random elements on the page
     var colours = ['#FFC926','#00DF70','#FA0032','#00EBFF'];
     var logos = ['lagoon','canyon','stadium','valley'];
-    var random = Math.floor(Math.random()*logos.length);
-    
+    var backgroundsCount = 46;
+    var random;
+    random = Math.floor(Math.random()*logos.length);
     $(".mainLogo").attr("src","logo/"+logos[random]+".png");
+    random = Math.floor(Math.random()*backgroundsCount);
+    $("html").attr("style","background-image: url('sitebackground/img ("+random+").jpg');");
 
     $( ".randomColour" ).each(function() {
         var random = Math.floor(Math.random()*logos.length);
         $( this ).css( "color", colours[random]);
-        console.log(colors[random]);
       });
-      
+    
 
 });
