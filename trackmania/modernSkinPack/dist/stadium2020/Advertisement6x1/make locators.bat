4b00796feaa7c028e75ba36b3ef45a38bce32fd5
marie <mariejuku@gmail.com>
2021-04-26 07:44:58 +1000

    pushed stadium 2020 6x1s

--- trackmania/modernSkinPack/dist/stadium2020/Advertisement6x1/make locators.bat
+++
@@ -1,5 +0,0 @@
-set host=https://mariejuku.github.io/trackmania/modernSkinPack/dist/stadium2020/Advertisement6x1/
-del *.tga.loc
-del *.webm.loc
-for /r %%a in (*.tga) do @echo %host%%%~nxa > %%~nxa.loc
-for /r %%a in (*.webm) do @echo %host%%%~nxa > %%~nxa.loc
