d967b059cbbaaa4370f5a93e1ecc4c9631c076cc
marie <mariejuku@gmail.com>
2021-09-15 00:49:39 +1000

    functional skin chooser data-driven fixes and file hosting

--- skinChooser.js
+++ skinChooser.js
@@ -1,47 +1,105 @@
 $( document ).ready(function() {
 
-	//consolidate skinItems
-	$('packData').find('skinPath').each(function(){
-		path = $(this).attr("src");
-		$(this).children("skinItem").each(function(){
-			file = $(this).attr("file");
-			$(this).attr("src",`${path}${file}`);
-			$(this).attr("name",`${file.split('.')[0]}`);
+	packButtonElement = $('elements>packButton>div');
+	skinButtonElement = $('elements>skinButton>div');
+	skinButtonRowElement = $('elements>skinButtonRow>div');
+	console.log(skinButtonRowElement);
+	zoom = 0;
+
+	skins = [];
+	skinGroups = {};
+	skinGroupNames = [];
+
+	packIds = [];
+	packs = {};
+	currentPack = undefined; //currently chosen pack
+	currentGroupAttribute = undefined; 
+	//collect packs data
+	$('packs>pack').each(function(){
+		var pack = {};
+		pack.id = $(this).attr("packid"); 
+		pack.name = $(this).attr("name");
+		pack.imageSrc = $(this).attr("image");
+		pack.defaultAttribute = $(this).attr("defaultAttribute");
+		//get pack skins
+		pack.skins = [];
+		$(this).find('skinPath').each(function(){
+			//figure out file stuff
+			fileSrc = $(this).attr('fileSrc');
+			fileExt = $(this).attr('fileExt');
+			fileExtVideo = $(this).attr('fileExtVideo');
+			thumbSrc = $(this).attr('thumbSrc');
+			thumbExt = $(this).attr('thumbExt');
+			thumbExtVideo = $(this).attr('thumbExtVideo');
+			if (thumbSrc == undefined) { thumbSrc = fileSrc;}
+			if (thumbExt == undefined) { thumbExt = fileExt;}
+			if (thumbExtVideo == undefined) { thumbExtVideo = fileExtVideo;}
+			//get attributes
+			pathCategory = $(this).attr('category');
+			pathBlock = $(this).attr('block');
+			pathColor = $(this).attr('color');
+			pathDirection = $(this).attr('direction');
+			pathPlatform = $(this).attr('platform');
+			pathEnvironment = $(this).attr('environment');
+			console.log(pathBlock);
+			$(this).find('skin').each(function(){
+				var skin = {}
+				skin.file = $(this).attr('file');
+				skin.name = skin.file;
+				skin.type = $(this).attr('type');
+				if (skin.type === 'video') {
+					skin.src = fileSrc + skin.file + fileExtVideo;
+					skin.thumbSrc = thumbSrc + skin.file + thumbExtVideo;
+				} else {
+					skin.src = fileSrc + skin.file + fileExt;
+					skin.thumbSrc = thumbSrc + skin.file + thumbExt;
+				}
+				
+				//inherit attributes
+				skin.category = pathCategory;
+				skin.block = pathBlock;
+				skin.color = pathColor;
+				skin.direction = pathDirection;
+				skin.platform = pathPlatform;
+				skin.environment = pathEnvironment;
+				
+				if ($(this).attr('category') != undefined) { skin.category = $(this).attr('category'); }
+				if ($(this).attr('block') != undefined) { skin.block = $(this).attr('block'); }
+				if ($(this).attr('color') != undefined) { skin.color = $(this).attr('color'); }
+				if ($(this).attr('direction') != undefined) { skin.direction = $(this).attr('direction'); }
+				if ($(this).attr('platform') != undefined) { skin.platform = $(this).attr('platform'); }
+				if ($(this).attr('environment') != undefined) { skin.environment = $(this).attr('environment'); }
+				//finish
+				pack.skins.push(skin);
+			});
 		});
+
+		packs[pack.id] = pack;
+		packIds.push(pack.id);
 	});
+	console.log(packs);
 
-	skinButtonHTML = $("skinChooserData .skinButtonContainer")[0].outerHTML;
-
-	spawnButton = function(skinItemElement) {
-		skinItemElement.append(skinButtonHTML);
-		src = skinItemElement.attr("src");
-		name = skinItemElement.attr("name");
-		skinItemElement.find(".skinButton").attr("src",`http://mariejuku.github.io/${src}`);
-		skinItemElement.find(".skinButton img").attr("src",src);
-		skinItemElement.find(".skinButton video").attr("src",src);
-		skinItemElement.find(".skinButton .skinName").html(name);
-		type = skinItemElement.attr("type");
-		if (type === "video") {
-			skinItemElement.find(".skinButton .video").removeClass('hidden');
-		} else {
-			skinItemElement.find(".skinButton .image").removeClass('hidden');
-		}
-		return skinItemElement.children(".skinButtonContainer");
-	}
+	//display pack buttons
+	packIds.forEach(packId => {
+		pack = packs[packId];
+		element = packButtonElement.clone();
+		element.find("button.packSelectButton").attr("packid",pack.id);
+		element.find(".label").html(pack.name);
+		element.find(".icon img").attr('src',pack.imageSrc);
+		$('.packButtons .buttonContainer').append(element);
+	});
 
     selectPack = function(packid) {
 		console.log(packid);
+		pack = packs[packid];
+		currentPack = pack;
 
-		$(".packTitle").html($(`.packSelectButton[packid="${packid}"]`).find(".label").html());
-		packData = $(`packData[packid="${packid}"]`);
-		//make category rows
-		packData.find('category').each(function(){
-			category = $(this).html();
-			$('packDisplay').append(`<category categoryid="${category}"><div class="buttonRow row no-gutters"></div></category>`);
-			$(`packData[packid="${packid}"] skinItem[category="${category}"]`).each(function(){
-				spawnButton($(this)).appendTo($(`packDisplay category[categoryid="${category}"] .buttonRow`));
-			});
-		});
+		$(".packTitle").html(pack.name);
+
+		groupBy(pack.defaultAttribute);
+
+		makeButtons();
+
 		// $("skinChooser .packloader").removeClass("hidden");
     	
 		// $(".packSelectButton").removeClass("selected");
@@ -64,6 +122,71 @@ $( document ).ready(function() {
 		// $("skinChooser .packloader").addClass("hidden");
     }
 
+	groupBy = function(attribute) {
+		currentGroupAttribute = attribute;
+
+		skinGroups = {}
+		skinGroupNames = []
+
+		pack.skins.forEach(skin => {
+			//make group if necessary
+			if (!(skin[attribute] in skinGroups)) {
+				skinGroups[skin[attribute]] = []
+				skinGroupNames.push(skin[attribute]);
+			}
+			skinGroups[skin[attribute]].push(skin);
+		});
+
+		console.log(skinGroups);
+		console.log(skinGroupNames);
+	}
+
+	makeButtons = function() {
+		//clear the content area
+		$('.skinButtons .content').html("");
+		//make skin buttons
+		skinGroupNames.forEach(skinGroupName => {
+			element = skinButtonRowElement.clone();
+			element.find(".rowTitle").html(skinGroupName);			
+
+			skinGroups[skinGroupName].forEach(skin => {
+				//make individual skin buttons
+				
+				element2 = skinButtonElement.clone();
+				element2.find(".skinName").html(skin.name);			
+				element2.find(".skinButton").attr("src",`http://mariejuku.github.io/${skin.src}`);
+
+				if (skin.type === "video") {
+					element2.find(".skinButton .video").removeClass('hidden');
+					element2.find(".skinButton video").attr("src",skin.thumbSrc);
+				} else {
+					element2.find(".skinButton .image").removeClass('hidden');
+					element2.find(".skinButton img").attr("src",skin.thumbSrc);
+				}
+
+				element.find('.rowContent').append(element2);
+				
+			});
+			$('.skinButtons .content').append(element);
+		});
+		//apply hooks to buttons
+		$("button.skinButton").click(function(){
+			copyTextToClipboard($(this).attr('src'));
+	
+			$("button.skinButton").each(function(){
+				$(this).find('.copyText').addClass("hidden");
+				$(this).find('.skinName').removeClass("hidden");
+			});
+			$(this).find('.skinName').addClass("hidden");
+			$(this).find('.copyText').removeClass("hidden");
+			var unhide = function(el){
+				el.find('.copyText').addClass("hidden");
+				el.find('.skinName').removeClass("hidden");
+			};
+			setTimeout(unhide,1000,$(this));
+		});
+	}
+
     // //generate available packs
     // $("skinChooserData > pack").each(function() {
 	// 	$("skinChooser packs").append(`<button type="button" class="btn packSelectButton" packid="${$(this).attr("packid")}"><div class="icon"><img src="${$(this).attr("img")}" height="44"/></div><div class="label">${$(this).attr("name")}</div></button>`)
@@ -73,48 +196,13 @@ $( document ).ready(function() {
     // $("skinChooser > .loader").addClass("hidden");
     // $("skinChooser > .main").removeClass("hidden");
 
-	// //apply hooks
-	// $(".packSelectButton").click(function(){
-	// 	selectPack($(this).attr("packid"));
-	// });
-
-    // //pre-select the first pack
-	//selectPack($(".packSelectButton:first-of-type").attr("packid"));
-	
-
-	
-	console.log(skinButtonHTML);
-	//spawn packs
-	$("packData").each(function(){
-		packid = $(this).attr("packid");
-		$("buttons").append(`<pack packid="${packid}"></pack>`);
-		//spawn buttons
-		
-	});
 	
 	//apply hooks to buttons
-	$(".skinButton").click(function(){
-		copyTextToClipboard($(this).attr('src'));
-
-		$(".skinButton").each(function(){
-			$(this).find('.copyText').addClass("hidden");
-			$(this).find('.skinName').removeClass("hidden");
-		});
-		$(this).find('.skinName').addClass("hidden");
-		$(this).find('.copyText').removeClass("hidden");
-		var unhide = function(el){
-			el.find('.copyText').addClass("hidden");
-			el.find('.skinName').removeClass("hidden");
-		};
-		setTimeout(unhide,1000,$(this));
+	$("button.packSelectButton").click(function(){
+		selectPack($(this).attr("packid"));
 	});
-	$(".packSelectButton").click(function(){
-		
-	});
-
-	selectPack("vtt");
-
 	
+	currentPack = selectPack("pride");
 
 	function copyTextToClipboard(text) {
 		var textArea = document.createElement("textarea");
