2ff9d10b36eacc5000741d612115b4e50f0532cb
marie <mariejuku@gmail.com>
2021-12-15 21:17:11 +1100

    added trackmania section

--- me/script.js
+++ me/script.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
+
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
     $('.dropdown-name button').click(function(){
         var string = $(this).html();
         $('.btn-name').html(string);
