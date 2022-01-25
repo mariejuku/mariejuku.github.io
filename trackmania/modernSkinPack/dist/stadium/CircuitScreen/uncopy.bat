e18dfbcc809c4886cdfddab61f557063738ae3d7
marie <mariejuku@gmail.com>
2022-01-25 14:37:57 +1100

    uploading stadium items

---
+++ trackmania/modernSkinPack/dist/stadium/CircuitScreen/uncopy.bat
@@ -0,0 +1,9 @@
+setlocal enabledelayedexpansion
+for %%a in (* copy.tga) do (
+  set file=%%a
+  ren "!file!" "!file: copy.tga=.tga!"
+)
+for %%a in (* copy.png) do (
+  set file=%%a
+  ren "!file!" "!file: copy.png=.png!"
+)
